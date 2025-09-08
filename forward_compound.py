885409f21e36870377e8f1f61a1f759a34f58fb3
constantinbender51-cmyk <constantinbender51@gmail.com>
2025-09-08 13:20:31 +0200

    Update forward_compound.py

--- forward_compound.py
+++ forward_compound.py
@@ -58,8 +58,8 @@ def run():
         # ---------- new signal ----------
         last50 = [dict(time=c["time"].timestamp(),o=c["open"],h=c["high"],l=c["low"],c=c["close"],v=c["volume"])
                   for c in candles[idx-50:idx]]
-        action, stop, target = get_signal(last50)
-        print("Action: ", action, "stop: ", stop, "target:", target);
+        action, stop, target, reason = get_signal(last50)
+        print("Action: ", action, "stop: ", stop, "target:", target, "reason: ", reason);
         if action != "FLAT":
             entry = bar["open"] * (1 + 5/3600/100)   # 5-sec slip
             slices.append(Slice(action.lower(), entry, stop, target, idx))
